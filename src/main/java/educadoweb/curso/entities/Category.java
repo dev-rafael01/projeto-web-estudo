a96dbaa441cd66e465cb58a915b7eb4e715363a6
rafael <rafaelsantos.js30@gmail.com>
2026-09-10 08:59:43 -0300

    assocition with JoinTable

--- src/main/java/educadoweb/curso/entities/Category.java
+++ src/main/java/educadoweb/curso/entities/Category.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 
 
 @Entity 
@@ -21,7 +24,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
-    @Transient 
+    
+    @JsonIgnore 
+    @ManyToMany(mappedBy = "categories") 
     private Set<Product> products = new HashSet<>();
 
    
