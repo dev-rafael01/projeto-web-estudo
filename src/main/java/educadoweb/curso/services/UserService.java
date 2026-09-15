4590623f4c78ac05f41081f36cb135cfe2fb9ada
rafael <rafaelsantos.js30@gmail.com>
2026-09-15 17:35:10 -0300

    Tratamento de exceção

--- src/main/java/educadoweb/curso/services/UserService.java
+++ src/main/java/educadoweb/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import educadoweb.curso.entities.User;
 import educadoweb.curso.repositories.UserRepository;
+import educadoweb.curso.services.exceptions.ResourceNotFoundException;
 
 @Service 
 public class UserService {
@@ -24,7 +25,7 @@ public class UserService {
     public User findByid(Long id)
     {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj)
